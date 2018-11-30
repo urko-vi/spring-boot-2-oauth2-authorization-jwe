279e78a8f11e198182d658f702cf53c9d57779a9
Urko Villanueva <uvillanueva@gfi.es>
2018-11-30 12:01:57 +0100

    Initial commit

---
+++ src/main/java/es/gfi/norte/oauthserver/OauthServerApplication.java
@@ -0,0 +1,12 @@
+package es.gfi.norte.oauthserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OauthServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OauthServerApplication.class, args);
+    }
+}
